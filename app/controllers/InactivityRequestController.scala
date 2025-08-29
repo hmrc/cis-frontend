fe48fce72df33523fd39001633198d9a699e3339
wg-hmrc <waigwa.gatamu@digital.hmrc.gov.uk>
2025-08-29 10:02:17 +0100

    [DTR-261] Fixing unit test

--- app/controllers/InactivityRequestController.scala
+++ app/controllers/InactivityRequestController.scala
@@ -44,10 +44,10 @@ class InactivityRequestController @Inject()(
 
   val form = formProvider()
 
-  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData) {
+  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
     implicit request =>
 
-      val preparedForm = request.userAnswers.flatMap(_.get(InactivityRequestPage)) match {
+      val preparedForm = request.userAnswers.get(InactivityRequestPage) match {
         case None => form
         case Some(value) => form.fill(value)
       }
