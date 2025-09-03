30afffb2ace9b5aa97675ba166904266a96efcef
wg-hmrc <waigwa.gatamu@digital.hmrc.gov.uk>
2025-09-03 12:28:31 +0100

    [DTR-275] Updating the Navigator

--- app/navigation/Navigator.scala
+++ app/navigation/Navigator.scala
@@ -21,23 +21,26 @@ import javax.inject.{Inject, Singleton}
 import play.api.mvc.Call
 import controllers.routes
 import pages._
+import pages.monthlyreturns.InactivityRequestPage
 import models._
 
 @Singleton
-class Navigator @Inject() () {
+class Navigator @Inject()() {
 
-  private val normalRoutes: Page => UserAnswers => Call = { case _ =>
-    _ => routes.IndexController.onPageLoad()
+  private val normalRoutes: Page => UserAnswers => Call = {
+    case InactivityRequestPage => _ => controllers.monthlyreturns.routes.ConfirmEmailAddressController.onPageLoad(NormalMode)
+    case _ => _ => routes.IndexController.onPageLoad()
   }
 
-  private val checkRouteMap: Page => UserAnswers => Call = { case _ =>
-    _ => routes.CheckYourAnswersController.onPageLoad()
+  private val checkRouteMap: Page => UserAnswers => Call = {
+    case InactivityRequestPage => _ => controllers.monthlyreturns.routes.ConfirmEmailAddressController.onPageLoad(CheckMode)
+    case _ => _ => routes.CheckYourAnswersController.onPageLoad()
   }
 
   def nextPage(page: Page, mode: Mode, userAnswers: UserAnswers): Call = mode match {
     case NormalMode =>
       normalRoutes(page)(userAnswers)
-    case CheckMode  =>
+    case CheckMode =>
       checkRouteMap(page)(userAnswers)
   }
 }
