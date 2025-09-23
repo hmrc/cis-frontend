e1fec90dc9912fabd49cf34e33fcb5e8bc998290
Sandip Khedekar <17478517+SandipKhedekar@users.noreply.github.com>
2025-09-23 16:44:23 +0100

    Code improvement

--- app/stub/controllers/monthlyreturns/StubSubmissionSendingController.scala
+++ app/stub/controllers/monthlyreturns/StubSubmissionSendingController.scala
@@ -17,24 +17,45 @@
 package stub.controllers.monthlyreturns
 
 import controllers.actions.*
+import models.UserAnswers
+import pages.monthlyreturns.{ConfirmEmailAddressPage, DateConfirmNilPaymentsPage, DeclarationPage, InactivityRequestPage}
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import stub.views.html.monthlyreturns.StubSubmissionSendingView
 
 import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
 
 class StubSubmissionSendingController @Inject() (
   override val messagesApi: MessagesApi,
+  sessionRepository: SessionRepository,
   identify: IdentifierAction,
   getData: DataRetrievalAction,
   requireData: DataRequiredAction,
   val controllerComponents: MessagesControllerComponents,
   view: StubSubmissionSendingView
-) extends FrontendBaseController
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
     with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
-    Ok(view())
+  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+
+    def removeUserAnswers(userAnswers: UserAnswers): Try[UserAnswers] = {
+      val pagesToRemove =
+        Seq(DateConfirmNilPaymentsPage, InactivityRequestPage, ConfirmEmailAddressPage, DeclarationPage)
+
+      pagesToRemove.foldLeft(Try(userAnswers)) { (currentUserAnswers, page) =>
+        currentUserAnswers.flatMap(_.remove(page))
+      }
+    }
+
+    for {
+      updatedUserAnswers <- Future.fromTry(removeUserAnswers(request.userAnswers))
+      _                  <- sessionRepository.set(updatedUserAnswers)
+    } yield Ok(view())
+
   }
 }
