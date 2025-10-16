64b529aff28fef5e31e5842ff5e16683527af133
jamalosman <7947206+jamalosman@users.noreply.github.com>
2025-10-16 16:01:14 +0100

    whitespace changes from scalafmt

--- app/controllers/PageNotFoundController.scala
+++ app/controllers/PageNotFoundController.scala
@@ -18,17 +18,19 @@ package controllers
 
 import config.FrontendAppConfig
 import javax.inject.Inject
-import play.api.i18n.{I18nSupport}
+import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.PageNotFoundView
 
-class PageNotFoundController @Inject()(
-    val controllerComponents: MessagesControllerComponents,
-    view: PageNotFoundView
-  )(implicit appConfig: FrontendAppConfig) extends FrontendBaseController with I18nSupport {
+class PageNotFoundController @Inject() (
+  val controllerComponents: MessagesControllerComponents,
+  view: PageNotFoundView
+)(implicit appConfig: FrontendAppConfig)
+    extends FrontendBaseController
+    with I18nSupport {
 
   def onPageLoad: Action[AnyContent] = Action { implicit request =>
-      Ok(view())
+    Ok(view())
   }
 }
--- test/views/monthlyreturns/AlreadySubmittedViewSpec.scala
+++ test/views/monthlyreturns/AlreadySubmittedViewSpec.scala
@@ -13,20 +13,20 @@ class AlreadySubmittedViewSpec extends SpecBase with Matchers {
 
     "must render the page with the correct heading and paragraph with link" in new Setup {
       val html = view()
-      val doc = Jsoup.parse(html.body)
+      val doc  = Jsoup.parse(html.body)
 
-      doc.title                                  must include(messages("monthlyreturns.alreadySubmitted.title"))
-      doc.select("h1").text                      must include(messages("monthlyreturns.alreadySubmitted.heading"))
-      doc.select("p").text                       must include(messages("monthlyreturns.alreadySubmitted.paragraph"))
-      doc.getElementsByClass("govuk-link").text  must include(messages("monthlyreturns.alreadySubmitted.link"))
+      doc.title                                 must include(messages("monthlyreturns.alreadySubmitted.title"))
+      doc.select("h1").text                     must include(messages("monthlyreturns.alreadySubmitted.heading"))
+      doc.select("p").text                      must include(messages("monthlyreturns.alreadySubmitted.paragraph"))
+      doc.getElementsByClass("govuk-link").text must include(messages("monthlyreturns.alreadySubmitted.link"))
     }
   }
 
   trait Setup {
-    val app = applicationBuilder().build()
-    val view = app.injector.instanceOf[AlreadySubmittedView]
+    val app                                       = applicationBuilder().build()
+    val view                                      = app.injector.instanceOf[AlreadySubmittedView]
     implicit val request: play.api.mvc.Request[_] = FakeRequest()
-    implicit val messages: Messages = play.api.i18n.MessagesImpl(
+    implicit val messages: Messages               = play.api.i18n.MessagesImpl(
       play.api.i18n.Lang.defaultLang,
       app.injector.instanceOf[play.api.i18n.MessagesApi]
     )
