c6c00959248f1a047547a46e491ba1da0d481af2
samraducan <231910514+samraducan@users.noreply.github.com>
2025-10-20 14:03:58 +0100

    DTR-574: Update messages.en and add unit tests

--- app/controllers/AccessDeniedController.scala
+++ app/controllers/AccessDeniedController.scala
@@ -17,26 +17,21 @@
 package controllers
 
 import config.FrontendAppConfig
-import controllers.actions.*
-
 import javax.inject.Inject
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.AccessDeniedView
 
-class AccessDeniedController @Inject()(
+class AccessDeniedController @Inject() (
   override val messagesApi: MessagesApi,
-  identify: IdentifierAction,
-  getData: DataRetrievalAction,
-  requireData: DataRequiredAction,
   val controllerComponents: MessagesControllerComponents,
   view: AccessDeniedView
-) (implicit appConfig: FrontendAppConfig)
-  extends FrontendBaseController with I18nSupport {
+)(implicit appConfig: FrontendAppConfig)
+    extends FrontendBaseController
+    with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
-    implicit request =>
-      Ok(view())
+  def onPageLoad: Action[AnyContent] = Action { implicit request =>
+    Ok(view())
   }
 }
