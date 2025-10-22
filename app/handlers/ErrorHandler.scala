80eb7851e685ccefbbc8d0d3011ec0b3b439807a
samraducan <231910514+samraducan@users.noreply.github.com>
2025-10-22 14:47:56 +0100

    DTR-886: Override default fallbackClientErrorTemplate

--- app/handlers/ErrorHandler.scala
+++ app/handlers/ErrorHandler.scala
@@ -22,14 +22,15 @@ import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.RequestHeader
 import play.twirl.api.Html
 import uk.gov.hmrc.play.bootstrap.frontend.http.FrontendErrorHandler
-import views.html.{ErrorTemplate, PageNotFoundView}
+import views.html.{AccessDeniedView, ErrorTemplate, PageNotFoundView}
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class ErrorHandler @Inject() (
   val messagesApi: MessagesApi,
   view: ErrorTemplate,
-  notFoundView: PageNotFoundView
+  notFoundView: PageNotFoundView,
+  accessDeniedView: AccessDeniedView
 )(implicit val ec: ExecutionContext, appConfig: FrontendAppConfig)
     extends FrontendErrorHandler
     with I18nSupport {
@@ -42,4 +43,6 @@ class ErrorHandler @Inject() (
   override def notFoundTemplate(implicit request: RequestHeader): Future[Html] =
     Future.successful(notFoundView())
 
+  override def fallbackClientErrorTemplate(implicit request: RequestHeader): Future[Html] =
+    Future.successful(accessDeniedView())
 }
