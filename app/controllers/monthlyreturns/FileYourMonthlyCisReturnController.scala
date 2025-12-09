ef4c36280a61abc7efa78606b8549198104965b0
Ashgeo86 <177663865+Ashgeo86@users.noreply.github.com>
2025-12-09 10:39:26 +0000

    DTR-1765 created controller and view

---
+++ app/controllers/monthlyreturns/FileYourMonthlyCisReturnController.scala
@@ -0,0 +1,24 @@
+package controllers.monthlyreturns
+
+import controllers.actions.*
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.FileYourMonthlyCisReturnView
+
+import javax.inject.Inject
+
+class FileYourMonthlyCisReturnController @Inject()(
+                                       override val messagesApi: MessagesApi,
+                                       identify: IdentifierAction,
+                                       getData: DataRetrievalAction,
+                                       requireData: DataRequiredAction,
+                                       val controllerComponents: MessagesControllerComponents,
+                                       view: FileYourMonthlyCisReturnView
+                                     ) extends FrontendBaseController with I18nSupport {
+
+  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
+    implicit request =>
+      Ok(view())
+  }
+}
