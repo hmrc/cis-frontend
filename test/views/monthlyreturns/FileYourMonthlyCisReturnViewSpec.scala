967cfb2297f5143392084d9d8b277a8c219e6622
Ashgeo86 <177663865+Ashgeo86@users.noreply.github.com>
2025-12-10 11:32:10 +0000

    DTR-1765 fixed tests and tidied up code

--- test/views/monthlyreturns/FileYourMonthlyCisReturnViewSpec.scala
+++ test/views/monthlyreturns/FileYourMonthlyCisReturnViewSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package views.monthlyreturns
 
 import base.SpecBase
@@ -15,7 +31,15 @@ class FileYourMonthlyCisReturnViewSpec extends SpecBase {
   "FileYourMonthlyCisReturnView" - {
     "must render the page with the correct html elements" in new Setup {
       val doc: Document = Jsoup.parse(html.toString)
+      doc.title             must include(messages("fileYourMonthlyCisReturn.title"))
+      doc.select("h1").text must include(messages("fileYourMonthlyCisReturn.heading"))
+      doc.select("p").text  must include(messages("fileYourMonthlyCisReturn.p1"))
+      doc.select("p").text  must include(messages("fileYourMonthlyCisReturn.p2"))
 
+      val startButton = doc.select("a.govuk-button--start")
+      startButton.isEmpty mustBe false
+      startButton.text must include("Start")
+      startButton.attr("href") mustBe "#"
     }
   }
 
