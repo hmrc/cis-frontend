6d5863ef2413c57a3bb47866c8caef21ffa8f891
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2025-12-23 10:54:54 +0000

    DTR-256 email length corrected

--- app/forms/monthlyreturns/ConfirmEmailAddressFormProvider.scala
+++ app/forms/monthlyreturns/ConfirmEmailAddressFormProvider.scala
@@ -24,7 +24,7 @@ import play.api.data.Form
 class ConfirmEmailAddressFormProvider @Inject() extends Mappings {
 
   private val EmailRegex     = "^[A-Za-z0-9!#$%&*+-/=?^_`{|}~.]+@[A-Za-z0-9!#$%&*+-/=?^_`{|}~.]+$"
-  private val MaxEmailLength = 132
+  private val MaxEmailLength = 154
 
   def apply(): Form[String] =
     Form(
