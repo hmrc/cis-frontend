7b37b3d6a156481db8e17e9fc9971ab51df1d471
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2025-12-23 11:00:23 +0000

    DTR-256 pr bot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,20 +3,20 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "10.4.0"
-  private val hmrcMongoVersion = "2.10.0"
+  private val hmrcMongoVersion = "2.11.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.20.0",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"    % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"            % hmrcMongoVersion
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.25.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
-    "org.scalatestplus"       %% "scalacheck-1-17"         % "3.2.18.0",
-    "org.mockito"              % "mockito-inline"          % "5.2.0"
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
+    "org.scalatestplus" %% "scalacheck-1-17"         % "3.2.18.0",
+    "org.mockito"        % "mockito-inline"          % "5.2.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
