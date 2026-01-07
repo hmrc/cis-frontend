4f1c8abd5881a4a9887d847cb94f5ccf085baba3
samraducan <231910514+samraducan@users.noreply.github.com>
2026-01-07 10:35:59 +0000

    DTR-2258: Initial page setup with scaffold

---
+++ test/controllers/monthlyreturns/SubcontractorsPaidThisMonthControllerSpec.scala
@@ -0,0 +1,29 @@
+package controllers.monthlyreturns
+
+import base.SpecBase
+import controllers.routes
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import views.html.SubcontractorsPaidThisMonthView
+
+class SubcontractorsPaidThisMonthControllerSpec extends SpecBase {
+
+  "SubcontractorsPaidThisMonth Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.SubcontractorsPaidThisMonthController.onPageLoad().url)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[SubcontractorsPaidThisMonthView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view()(request, messages(application)).toString
+      }
+    }
+  }
+}
