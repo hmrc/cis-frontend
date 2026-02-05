f727cb1489ddf29e3c4aa43949741fc6f6ab038c
samraducan <231910514+samraducan@users.noreply.github.com>
2026-02-05 15:53:41 +0000

    DTR-2832: Initial scaffold and move files in monthly returns folder

---
+++ test/controllers/monthlyreturns/CheckAnswersTotalPaymentsControllerSpec.scala
@@ -0,0 +1,29 @@
+package controllers.monthlyreturns
+
+import base.SpecBase
+import controllers.routes
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import views.html.monthlyreturns.CheckAnswersTotalPaymentsView
+
+class CheckAnswersTotalPaymentsControllerSpec extends SpecBase {
+
+  "CheckAnswersTotalPayments Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, controllers.monthlyreturns.routes.CheckAnswersTotalPaymentsController.onPageLoad().url)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[CheckAnswersTotalPaymentsView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view()(request, messages(application)).toString
+      }
+    }
+  }
+}
