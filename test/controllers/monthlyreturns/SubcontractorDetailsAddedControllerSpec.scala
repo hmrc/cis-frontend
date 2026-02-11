1173543c7ec2a8037fdc8dece1b9670a3e61c12c
yanshengzhangHMRC <171041010+yanshengzhangHMRC@users.noreply.github.com>
2026-02-11 14:51:37 +0000

    [DTR-2831] PR comments 2

--- test/controllers/monthlyreturns/SubcontractorDetailsAddedControllerSpec.scala
+++ test/controllers/monthlyreturns/SubcontractorDetailsAddedControllerSpec.scala
@@ -17,7 +17,7 @@
 package controllers.monthlyreturns
 
 import base.SpecBase
-import models.UserAnswers
+import models.{NormalMode, UserAnswers}
 import play.api.libs.json.Json
 import play.api.test.FakeRequest
 import play.api.test.Helpers.*
@@ -141,6 +141,174 @@ class SubcontractorDetailsAddedControllerSpec extends SpecBase {
         contentAsString(result) must include(
           "You have not entered payment details for all of your selected subcontractors"
         )
+      }
+    }
+
+    "must redirect to SystemError on POST when builder returns None" in {
+
+      val ua = UserAnswers(
+        id = userAnswersId,
+        data = Json.obj("subcontractors" -> Json.arr()), // builder None
+        lastUpdated = Instant.now
+      )
+
+      val application =
+        applicationBuilder(userAnswers = Some(ua))
+          .configure(
+            "play.http.router" -> "testOnly.TestRoutes",
+            "features.welsh-translation" -> false,
+            "timeout-dialog.timeout" -> 900,
+            "timeout-dialog.countdown" -> 120,
+            "contact-frontend.serviceId" -> "cis-frontend",
+            "host" -> "http://localhost"
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, "/monthly-return/subcontractor-details-added")
+            .withFormUrlEncodedBody("value" -> "true")
+
+        val result = route(application, request).value
+
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result).value mustBe controllers.routes.SystemErrorController.onPageLoad().url
+      }
+    }
+
+    "must return BadRequest on POST when form has errors" in {
+
+      val ua = UserAnswers(
+        id = userAnswersId,
+        data = Json.obj(
+          "subcontractors" -> Json.arr(
+            Json.obj(
+              "subcontractorId" -> 1001L,
+              "name" -> "TyneWear Ltd",
+              "totalPaymentsMade" -> 1000.00,
+              "costOfMaterials" -> 200.00,
+              "totalTaxDeducted" -> 200.00
+            )
+          )
+        ),
+        lastUpdated = Instant.now
+      )
+
+      val application =
+        applicationBuilder(userAnswers = Some(ua))
+          .configure(
+            "play.http.router" -> "testOnly.TestRoutes",
+            "features.welsh-translation" -> false,
+            "timeout-dialog.timeout" -> 900,
+            "timeout-dialog.countdown" -> 120,
+            "contact-frontend.serviceId" -> "cis-frontend",
+            "host" -> "http://localhost"
+          )
+          .build()
+
+      running(application) {
+        val request = FakeRequest(POST, "/monthly-return/subcontractor-details-added")
+
+        val result = route(application, request).value
+
+        status(result) mustBe BAD_REQUEST
+      }
+    }
+
+    "must redirect on POST Yes when viewModel.hasIncomplete = false" in {
+
+      val ua = UserAnswers(
+        id = userAnswersId,
+        data = Json.obj(
+          "subcontractors" -> Json.arr(
+            Json.obj(
+              "subcontractorId" -> 1001L,
+              "name" -> "Complete Ltd",
+              "totalPaymentsMade" -> 1000.00,
+              "costOfMaterials" -> 200.00,
+              "totalTaxDeducted" -> 200.00
+            ),
+            Json.obj(
+              "subcontractorId" -> 1002L,
+              "name" -> "Also Complete Ltd",
+              "totalPaymentsMade" -> 500.00,
+              "costOfMaterials" -> 50.00,
+              "totalTaxDeducted" -> 50.00
+            )
+          )
+        ),
+        lastUpdated = Instant.now
+      )
+
+      val application =
+        applicationBuilder(userAnswers = Some(ua))
+          .configure(
+            "play.http.router" -> "testOnly.TestRoutes",
+            "features.welsh-translation" -> false,
+            "timeout-dialog.timeout" -> 900,
+            "timeout-dialog.countdown" -> 120,
+            "contact-frontend.serviceId" -> "cis-frontend",
+            "host" -> "http://localhost"
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, "/monthly-return/subcontractor-details-added")
+            .withFormUrlEncodedBody("value" -> "true")
+
+        val result = route(application, request).value
+
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result).value mustBe
+          controllers.monthlyreturns.routes.SubcontractorDetailsAddedController
+            .onPageLoad(NormalMode)
+            .url
+      }
+    }
+
+    "must redirect on POST No (answer == false)" in {
+
+      val ua = UserAnswers(
+        id = userAnswersId,
+        data = Json.obj(
+          "subcontractors" -> Json.arr(
+            Json.obj(
+              "subcontractorId" -> 1001L,
+              "name" -> "Complete Ltd",
+              "totalPaymentsMade" -> 1000.00,
+              "costOfMaterials" -> 200.00,
+              "totalTaxDeducted" -> 200.00
+            )
+          )
+        ),
+        lastUpdated = Instant.now
+      )
+
+      val application =
+        applicationBuilder(userAnswers = Some(ua))
+          .configure(
+            "play.http.router" -> "testOnly.TestRoutes",
+            "features.welsh-translation" -> false,
+            "timeout-dialog.timeout" -> 900,
+            "timeout-dialog.countdown" -> 120,
+            "contact-frontend.serviceId" -> "cis-frontend",
+            "host" -> "http://localhost"
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, "/monthly-return/subcontractor-details-added")
+            .withFormUrlEncodedBody("value" -> "false")
+
+        val result = route(application, request).value
+
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result).value mustBe
+          controllers.monthlyreturns.routes.SubcontractorDetailsAddedController
+            .onPageLoad(NormalMode)
+            .url
       }
     }
   }
--- test/forms/monthlyreturns/SubcontractorDetailsAddedFormProviderSpec.scala
+++ test/forms/monthlyreturns/SubcontractorDetailsAddedFormProviderSpec.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package forms
+package forms.monthlyreturns
 
 import forms.behaviours.BooleanFieldBehaviours
 import forms.monthlyreturns.SubcontractorDetailsAddedFormProvider
