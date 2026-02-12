baddb2a073ab00577cbfeb839943bdc10fcb80f5
wg-hmrc <107496826+wg-hmrc@users.noreply.github.com>
2026-02-12 15:54:52 +0000

    [DTR-2830] CIS MRF Screen - Which Subcontractors do you need to add details for? (#100)
    * [DTR-2830] scaffolding page added
    
    * [DTR-2830] Move AddSubcontractorDetails into monthlyreturns domain
    
    * [DTR-2830] Added unit test for the view
    
    * [DTR-2830] Resoloved PR comments
    
    * [DTR-2830] Use SelectedSubcontractor for dynamic subcontractor radios
    
    * [DTR-2830] Fixing unit tests
    
    * [DTR-2830] AddSubcontractorDetailsControllerSpec fixed
    
    * [DTR-2830]  coverage for radioItems added
    
    * [DTR-2830] Fix error summary link scrolling

---
+++ test/controllers/AddSubcontractorDetailsControllerSpec.scala
@@ -0,0 +1,216 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import forms.monthlyreturns.AddSubcontractorDetailsFormProvider
+import models.monthlyreturns.SelectedSubcontractor
+import models.{NormalMode, UserAnswers}
+import navigation.{FakeNavigator, Navigator}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import pages.monthlyreturns.AddSubcontractorDetailsPage
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import repositories.SessionRepository
+import views.html.monthlyreturns.AddSubcontractorDetailsView
+
+import scala.concurrent.Future
+
+class AddSubcontractorDetailsControllerSpec extends SpecBase with MockitoSugar {
+
+  def onwardRoute = Call("GET", "/foo")
+
+  lazy val addSubcontractorDetailsRoute =
+    controllers.monthlyreturns.routes.AddSubcontractorDetailsController.onPageLoad(NormalMode).url
+
+  val formProvider = new AddSubcontractorDetailsFormProvider()
+  val form         = formProvider()
+
+  private val subcontractorsWithDetails: Seq[SelectedSubcontractor] =
+    Seq(
+      SelectedSubcontractor(
+        id = 1L,
+        name = "BuildRight Construction",
+        totalPaymentsMade = None,
+        costOfMaterials = None,
+        totalTaxDeducted = None
+      )
+    )
+
+  private val subcontractorsWithoutDetails: Seq[SelectedSubcontractor] =
+    Seq(
+      SelectedSubcontractor(
+        id = 2L,
+        name = "Northern Trades Ltd",
+        totalPaymentsMade = None,
+        costOfMaterials = None,
+        totalTaxDeducted = None
+      ),
+      SelectedSubcontractor(
+        id = 3L,
+        name = "TyneWear Ltd",
+        totalPaymentsMade = None,
+        costOfMaterials = None,
+        totalTaxDeducted = None
+      )
+    )
+
+  "AddSubcontractorDetails Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, addSubcontractorDetailsRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[AddSubcontractorDetailsView]
+
+        status(result) mustEqual OK
+        val content = contentAsString(result)
+
+        content mustEqual view(form, NormalMode, subcontractorsWithDetails, subcontractorsWithoutDetails)(
+          request,
+          messages(application)
+        ).toString
+
+        content must include("BuildRight Construction")
+        content must include("Northern Trades Ltd")
+        content must include("TyneWear Ltd")
+      }
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val selectedId  = subcontractorsWithoutDetails.head.id.toString
+      val userAnswers =
+        UserAnswers(userAnswersId).set(AddSubcontractorDetailsPage, selectedId).success.value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, addSubcontractorDetailsRoute)
+
+        val view = application.injector.instanceOf[AddSubcontractorDetailsView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(
+          form.fill(selectedId),
+          NormalMode,
+          subcontractorsWithDetails,
+          subcontractorsWithoutDetails
+        )(
+          request,
+          messages(application)
+        ).toString
+      }
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+          .overrides(
+            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, addSubcontractorDetailsRoute)
+            .withFormUrlEncodedBody(("value", subcontractorsWithoutDetails.head.id.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onwardRoute.url
+      }
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, addSubcontractorDetailsRoute)
+            .withFormUrlEncodedBody(("value", "invalid value"))
+
+        val boundForm = form.bind(Map("value" -> "invalid value"))
+
+        val view = application.injector.instanceOf[AddSubcontractorDetailsView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) mustEqual view(
+          boundForm,
+          NormalMode,
+          subcontractorsWithDetails,
+          subcontractorsWithoutDetails
+        )(
+          request,
+          messages(application)
+        ).toString
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request = FakeRequest(GET, addSubcontractorDetailsRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "redirect to Journey Recovery for a POST if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, addSubcontractorDetailsRoute)
+            .withFormUrlEncodedBody(("value", subcontractorsWithoutDetails.head.id.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+  }
+}
