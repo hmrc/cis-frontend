3593bdcaebe91d3153bb8796ffc401af6a1dd48f
Jesse-Murray-Dev <209797357+Jesse-Murray-Dev@users.noreply.github.com>
2026-02-12 10:52:59 +0000

    Fixed Test name

--- test/viewmodels/govuk/checkAnswers/monthlyReturns/PaymentDetailsConfirmationSummarySpec.scala
+++ test/viewmodels/govuk/checkAnswers/monthlyReturns/PaymentDetailsConfirmationSummarySpec.scala
@@ -29,7 +29,7 @@ class PaymentDetailsConfirmationSummarySpec extends SpecBase with OptionValues {
 
   private implicit val messages: Messages = Helpers.stubMessages()
 
-  "ConfirmationByEmailSummary" - {
+  "PaymentDetailsConfirmationSummary" - {
 
     "when answer is present" - {
 
