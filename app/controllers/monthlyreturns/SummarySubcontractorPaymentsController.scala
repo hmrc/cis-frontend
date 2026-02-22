d7e1a00acef081ad6e903fc021536aa83d7d4f7d
wg-hmrc <waigwa.gatamu@digital.hmrc.gov.uk>
2026-02-22 20:52:34 +0000

    [DTR-2833] implement summary subcontractor payments page

--- app/controllers/monthlyreturns/SummarySubcontractorPaymentsController.scala
+++ app/controllers/monthlyreturns/SummarySubcontractorPaymentsController.scala
@@ -18,29 +18,50 @@ package controllers.monthlyreturns
 
 import controllers.actions._
 import javax.inject.Inject
+import models.UserAnswers
+import pages.monthlyreturns.{SelectedSubcontractorPage, SummaryTotalCisDeductionsPage, SummaryTotalMaterialsCostPage, SummaryTotalPaymentsPage}
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.monthlyreturns.SummarySubcontractorPaymentsView
 
+import scala.concurrent.{ExecutionContext, Future}
 import scala.math.BigDecimal.RoundingMode
 
 class SummarySubcontractorPaymentsController @Inject() (
   override val messagesApi: MessagesApi,
+  sessionRepository: SessionRepository,
   identify: IdentifierAction,
   getData: DataRetrievalAction,
   requireData: DataRequiredAction,
   val controllerComponents: MessagesControllerComponents,
   view: SummarySubcontractorPaymentsView
-) extends FrontendBaseController
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
     with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
-    val subcontractorCount = 2
-    val totalPayments      = BigDecimal(3600).setScale(2, RoundingMode.HALF_UP)
-    val totalMaterialsCost = BigDecimal(900).setScale(2, RoundingMode.HALF_UP)
-    val totalCisDeductions = BigDecimal(540).setScale(2, RoundingMode.HALF_UP)
+  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    val subcontractors = request.userAnswers
+      .get(SelectedSubcontractorPage.all)
+      .getOrElse(Map.empty)
+      .values
+      .filter(s => s.totalPaymentsMade.isDefined && s.costOfMaterials.isDefined && s.totalTaxDeducted.isDefined)
+      .toSeq
 
-    Ok(view(subcontractorCount, totalPayments, totalMaterialsCost, totalCisDeductions))
+    val totalPayments      = subcontractors.flatMap(_.totalPaymentsMade).sum.setScale(2, RoundingMode.HALF_DOWN)
+    val totalMaterialsCost = subcontractors.flatMap(_.costOfMaterials).sum.setScale(2, RoundingMode.HALF_DOWN)
+    val totalCisDeductions = subcontractors.flatMap(_.totalTaxDeducted).sum.setScale(2, RoundingMode.HALF_DOWN)
+    val subcontractorCount = subcontractors.size
+
+    for {
+      updatedAnswers <- Future.fromTry(
+                          request.userAnswers
+                            .set(SummaryTotalPaymentsPage, totalPayments)
+                            .flatMap(_.set(SummaryTotalMaterialsCostPage, totalMaterialsCost))
+                            .flatMap(_.set(SummaryTotalCisDeductionsPage, totalCisDeductions))
+                        )
+      _              <- sessionRepository.set(updatedAnswers)
+    } yield Ok(view(subcontractorCount, totalPayments, totalMaterialsCost, totalCisDeductions))
   }
 }
--- test/controllers/monthlyreturns/SummarySubcontractorPaymentsControllerSpec.scala
+++ test/controllers/monthlyreturns/SummarySubcontractorPaymentsControllerSpec.scala
@@ -17,32 +17,130 @@
 package controllers.monthlyreturns
 
 import base.SpecBase
+import models.monthlyreturns.SelectedSubcontractor
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import pages.monthlyreturns.SelectedSubcontractorPage
+import play.api.inject.bind
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
+import repositories.SessionRepository
 import views.html.monthlyreturns.SummarySubcontractorPaymentsView
-import scala.math.BigDecimal
 
-class SummarySubcontractorPaymentsControllerSpec extends SpecBase {
+import scala.concurrent.Future
+import scala.math.BigDecimal.RoundingMode
+
+class SummarySubcontractorPaymentsControllerSpec extends SpecBase with MockitoSugar {
+
+  private val mockSessionRepository = mock[SessionRepository]
+
+  private def buildUserAnswers(subcontractors: Map[Int, SelectedSubcontractor]) =
+    subcontractors.foldLeft(emptyUserAnswers) { case (ua, (index, sub)) =>
+      ua.set(SelectedSubcontractorPage(index), sub).success.value
+    }
+
+  lazy val summaryRoute =
+    controllers.monthlyreturns.routes.SummarySubcontractorPaymentsController.onPageLoad().url
 
   "SummarySubcontractorPayments Controller" - {
 
-    "must return OK and the correct view for a GET" in {
+    "must return OK and render totals calculated from subcontractors with complete details" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val subcontractors = Map(
+        1 -> SelectedSubcontractor(101, "Alpha Ltd",  Some(BigDecimal(1000)), Some(BigDecimal(200)), Some(BigDecimal(160))),
+        2 -> SelectedSubcontractor(102, "Beta Ltd",   Some(BigDecimal(2000)), Some(BigDecimal(400)), Some(BigDecimal(320))),
+        3 -> SelectedSubcontractor(103, "Gamma Ltd",  Some(BigDecimal(600)),  Some(BigDecimal(300)), Some(BigDecimal(60)))
+      )
+
+      val userAnswers = buildUserAnswers(subcontractors)
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers))
+        .overrides(bind[SessionRepository].toInstance(mockSessionRepository))
+        .build()
 
       running(application) {
-        val request =
-          FakeRequest(GET, controllers.monthlyreturns.routes.SummarySubcontractorPaymentsController.onPageLoad().url)
+        val request = FakeRequest(GET, summaryRoute)
+        val result  = route(application, request).value
+        val view    = application.injector.instanceOf[SummarySubcontractorPaymentsView]
 
-        val result = route(application, request).value
-
-        val view = application.injector.instanceOf[SummarySubcontractorPaymentsView]
+        val expectedPayments      = BigDecimal(3600).setScale(2, RoundingMode.HALF_DOWN)
+        val expectedMaterialsCost = BigDecimal(900).setScale(2, RoundingMode.HALF_DOWN)
+        val expectedCisDeductions = BigDecimal(540).setScale(2, RoundingMode.HALF_DOWN)
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(2, BigDecimal(3600), BigDecimal(900), BigDecimal(540))(
+        contentAsString(result) mustEqual view(3, expectedPayments, expectedMaterialsCost, expectedCisDeductions)(
           request,
           messages(application)
         ).toString
+      }
+    }
+
+    "must exclude subcontractors with incomplete details from totals" in {
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val subcontractors = Map(
+        1 -> SelectedSubcontractor(101, "Alpha Ltd", Some(BigDecimal(1000)), Some(BigDecimal(200)), Some(BigDecimal(160))),
+        2 -> SelectedSubcontractor(102, "Beta Ltd",  None,                  None,                  None)
+      )
+
+      val userAnswers = buildUserAnswers(subcontractors)
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers))
+        .overrides(bind[SessionRepository].toInstance(mockSessionRepository))
+        .build()
+
+      running(application) {
+        val request = FakeRequest(GET, summaryRoute)
+        val result  = route(application, request).value
+        val view    = application.injector.instanceOf[SummarySubcontractorPaymentsView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(
+          1,
+          BigDecimal(1000).setScale(2, RoundingMode.HALF_DOWN),
+          BigDecimal(200).setScale(2, RoundingMode.HALF_DOWN),
+          BigDecimal(160).setScale(2, RoundingMode.HALF_DOWN)
+        )(request, messages(application)).toString
+      }
+    }
+
+    "must return OK with zero totals when no subcontractors have complete details" in {
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        .overrides(bind[SessionRepository].toInstance(mockSessionRepository))
+        .build()
+
+      running(application) {
+        val request = FakeRequest(GET, summaryRoute)
+        val result  = route(application, request).value
+        val view    = application.injector.instanceOf[SummarySubcontractorPaymentsView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(
+          0,
+          BigDecimal(0).setScale(2, RoundingMode.HALF_DOWN),
+          BigDecimal(0).setScale(2, RoundingMode.HALF_DOWN),
+          BigDecimal(0).setScale(2, RoundingMode.HALF_DOWN)
+        )(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request = FakeRequest(GET, summaryRoute)
+        val result  = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
       }
     }
   }
