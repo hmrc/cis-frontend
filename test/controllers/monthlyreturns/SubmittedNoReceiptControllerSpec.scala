5dd32c623de55c22a8697a9c3bd92715ac2d909c
samraducan <231910514+samraducan@users.noreply.github.com>
2026-03-04 12:10:13 +0000

    DTR-2841: Update page content and unit tests for existing files as per prototype

--- test/controllers/monthlyreturns/SubmittedNoReceiptControllerSpec.scala
+++ test/controllers/monthlyreturns/SubmittedNoReceiptControllerSpec.scala
@@ -18,8 +18,8 @@ package controllers.monthlyreturns
 
 import base.SpecBase
 import controllers.monthlyreturns
-import models.UserAnswers
-import pages.monthlyreturns.{ConfirmEmailAddressPage, ContractorNamePage, DateConfirmNilPaymentsPage}
+import models.{ReturnType, UserAnswers}
+import pages.monthlyreturns.{ConfirmEmailAddressPage, ContractorNamePage, DateConfirmNilPaymentsPage, ReturnTypePage}
 import play.api.Application
 import play.api.inject.bind
 import play.api.mvc.AnyContentAsEmpty
@@ -29,86 +29,151 @@ import views.html.monthlyreturns.SubmittedNoReceiptView
 
 import java.time.format.DateTimeFormatter
 import java.time.{Clock, Instant, LocalDate, ZoneId, ZoneOffset, ZonedDateTime}
+import java.util.Locale
 
 class SubmittedNoReceiptControllerSpec extends SpecBase {
 
-  "SubmittedNoReceipt Controller" - {
+  val email: String              = "test@test.com"
+  val periodEnd: LocalDate       = LocalDate.of(2018, 3, 5)
+  val fixedInstant: Instant      = Instant.parse("2017-01-06T08:46:00Z")
+  val contractorName: String     = "PAL 355 Scheme"
+  val employerRef: String        = "taxOfficeNumber/taxOfficeReference"
+  val submissionType: ReturnType = ReturnType.MonthlyNilReturn
 
-    "must return OK and render the expected view" in new Setup {
-      running(app) {
-        val result = route(app, request).value
+  private val dmyFmt  = DateTimeFormatter.ofPattern("MMMM uuuu").withLocale(Locale.UK)
+  private val timeFmt = DateTimeFormatter.ofPattern("h:mma").withLocale(Locale.UK)
+  private val london  = ZoneId.of("Europe/London")
 
-        status(result) mustBe OK
-        contentAsString(result) mustBe expectedHtml
+  protected lazy val ukNow: ZonedDateTime =
+    ZonedDateTime.ofInstant(fixedInstant, london)
+
+  protected lazy val submittedTime: String =
+    ukNow.format(timeFmt).toLowerCase
+
+  protected lazy val submittedDate: String =
+    ukNow.format(DateTimeFormatter.ofPattern("d MMMM uuuu"))
+
+  val baseUa: UserAnswers =
+    userAnswersWithCisId
+      .set(ContractorNamePage, contractorName)
+      .success
+      .value
+      .set(ConfirmEmailAddressPage, email)
+      .success
+      .value
+      .set(DateConfirmNilPaymentsPage, periodEnd)
+      .success
+      .value
+      .set(ReturnTypePage, submissionType)
+      .success
+      .value
+
+  lazy val request =
+    FakeRequest(GET, routes.SubmittedNoReceiptController.onPageLoad.url)
+
+  "SubmittedNoReceiptController" - {
+
+    "onPageLoad" - {
+
+      "must return OK and render the expected view" in {
+
+        val app =
+          applicationBuilder(userAnswers = Some(baseUa))
+            .overrides(bind[Clock].toInstance(Clock.fixed(fixedInstant, ZoneOffset.UTC)))
+            .build()
+
+        val view = app.injector.instanceOf[SubmittedNoReceiptView]
+
+        val expectedHtml =
+          view(
+            periodEnd = periodEnd.format(dmyFmt),
+            submittedTime = submittedTime,
+            submittedDate = submittedDate,
+            contractorName = contractorName,
+            empRef = employerRef,
+            email = email,
+            submissionType = submissionType
+          )(request, messages(app)).toString
+
+        running(app) {
+          val result = route(app, request).value
+          status(result) mustBe OK
+          contentAsString(result) mustBe expectedHtml
+        }
+      }
+
+      "must redirect to Unauthorised Organisation Affinity if cisId missing" in {
+
+        val app = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+        running(app) {
+          val result = route(app, request).value
+          status(result) mustBe SEE_OTHER
+          redirectLocation(result).value mustBe
+            controllers.routes.UnauthorisedOrganisationAffinityController.onPageLoad().url
+        }
+      }
+
+      "must throw if contractorName missing" in {
+
+        val incompleteUa =
+          userAnswersWithCisId
+            .set(DateConfirmNilPaymentsPage, periodEnd)
+            .success
+            .value
+            .set(ReturnTypePage, submissionType)
+            .success
+            .value
+
+        val app = applicationBuilder(userAnswers = Some(incompleteUa)).build()
+
+        running(app) {
+          val thrown = intercept[IllegalStateException] {
+            await(route(app, request).get)
+          }
+          thrown.getMessage must include("contractorName missing")
+        }
+      }
+
+      "must throw if taxPeriodEnd is missing" in {
+
+        val incompleteUa =
+          userAnswersWithCisId
+            .set(ContractorNamePage, contractorName)
+            .success
+            .value
+            .set(ConfirmEmailAddressPage, email)
+            .success
+            .value
+            .set(ReturnTypePage, submissionType)
+            .success
+            .value
+
+        val app = applicationBuilder(userAnswers = Some(incompleteUa)).build()
+
+        running(app) {
+          val thrown = intercept[IllegalStateException] {
+            await(route(app, request).get)
+          }
+          thrown.getMessage must include("taxPeriodEnd missing")
+        }
+      }
+
+      "must redirect to SystemErrorController if employerReference missing" in {
+
+        val app =
+          applicationBuilder(
+            userAnswers = Some(baseUa),
+            hasEmployeeRef = false
+          ).build()
+
+        running(app) {
+          val result = route(app, request).value
+          status(result) mustBe SEE_OTHER
+          redirectLocation(result).value mustBe
+            controllers.routes.SystemErrorController.onPageLoad().url
+        }
       }
     }
-
-    "must redirect to Unauthorised Organisation Affinity if cisId is not found in UserAnswer" in new Setup {
-
-      private val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
-
-      running(application) {
-
-        val result = route(application, request).value
-
-        status(result) mustEqual SEE_OTHER
-
-        redirectLocation(
-          result
-        ).value mustEqual controllers.routes.UnauthorisedOrganisationAffinityController.onPageLoad().url
-      }
-    }
-  }
-
-  trait Setup {
-    val email: String          = "test@test.com"
-    val periodEnd: LocalDate   = LocalDate.of(2018, 3, 5)
-    val fixedInstant: Instant  = Instant.parse("2017-01-06T08:46:00Z")
-    val contractorName: String = "PAL 355 Scheme"
-    val employerRef: String    = "taxOfficeNumber/taxOfficeReference"
-
-    private val dmyFmt  = DateTimeFormatter.ofPattern("d MMM uuuu")
-    private val timeFmt = DateTimeFormatter.ofPattern("HH:mm z")
-    private val london  = ZoneId.of("Europe/London")
-
-    protected lazy val ukNow: ZonedDateTime =
-      ZonedDateTime.ofInstant(fixedInstant, london)
-
-    protected lazy val submittedTime: String =
-      ukNow.format(timeFmt)
-
-    protected lazy val submittedDate: String =
-      ukNow.format(dmyFmt)
-
-    lazy val ua: UserAnswers =
-      userAnswersWithCisId
-        .set(ContractorNamePage, contractorName)
-        .success
-        .value
-        .set(ConfirmEmailAddressPage, email)
-        .success
-        .value
-        .set(DateConfirmNilPaymentsPage, periodEnd)
-        .success
-        .value
-
-    lazy val app: Application =
-      applicationBuilder(userAnswers = Some(ua))
-        .overrides(bind[Clock].toInstance(Clock.fixed(fixedInstant, ZoneOffset.UTC)))
-        .build()
-
-    lazy val request: FakeRequest[AnyContentAsEmpty.type] =
-      FakeRequest(GET, routes.SubmittedNoReceiptController.onPageLoad.url)
-    lazy val view: SubmittedNoReceiptView                 = app.injector.instanceOf[SubmittedNoReceiptView]
-
-    lazy val expectedHtml: String =
-      view(
-        periodEnd = periodEnd.format(dmyFmt),
-        submittedTime = submittedTime,
-        submittedDate = submittedDate,
-        contractorName = contractorName,
-        empRef = employerRef,
-        email = email
-      )(request, messages(app)).toString
   }
 }
--- test/views/monthlyreturns/SubmittedNoReceiptViewSpec.scala
+++ test/views/monthlyreturns/SubmittedNoReceiptViewSpec.scala
@@ -17,6 +17,7 @@
 package views.monthlyreturns
 
 import base.SpecBase
+import models.ReturnType
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import play.api.Application
@@ -31,61 +32,101 @@ import java.time.{Instant, LocalDate, ZoneId, ZonedDateTime}
 class SubmittedNoReceiptViewSpec extends SpecBase {
 
   "SubmittedNoReceiptView" - {
+
     "must render the correct content on the page" in new Setup {
-      val html: HtmlFormat.Appendable = view(periodEnd, submittedTime, submittedDate, contractorName, empRef, email)
-      val doc: Document               = Jsoup.parse(html.body)
 
-      doc.title             must include(messages("submittedNoReceipt.title"))
-      doc.select("h1").text must include(messages("submittedNoReceipt.heading"))
-      doc.select("p").text  must include(
+      val doc: Document = Jsoup.parse(html.toString)
+
+      doc.title must include(messages("monthlyreturns.submittedNoReceipt.title"))
+
+      doc.select(".govuk-panel__title").text must include(
+        messages("monthlyreturns.submittedNoReceipt.heading")
+      )
+
+      doc.select("p.govuk-body").text must include(
+        messages("monthlyreturns.submittedNoReceipt.p1", submittedTime, submittedDate)
+      )
+
+      doc.select("h2").text must include(
+        messages("monthlyreturns.submittedNoReceipt.details.h2")
+      )
+
+      val summaryText: String = doc.select(".govuk-summary-list").text()
+      summaryText must include(contractorName)
+      summaryText must include(empRef)
+      summaryText must include(messages(s"monthlyreturns.returnType.${submissionType.toString}"))
+      summaryText must include(periodEnd)
+
+      doc.select("p.govuk-body").text must include(
         messages(
-          s"Your CIS monthly return for period ended $periodEnd was successfully submitted to HMRC at $submittedTime on $submittedDate"
+          "monthlyreturns.submittedNoReceipt.confirmationOfSuccessfulSubmission",
+          email
         )
       )
-      doc.select("h2").text must include(messages("submittedNoReceipt.details.h2"))
-      doc.select("dt").text must include(messages("submittedNoReceipt.details.contractorName"))
-      doc.select("dd").text must include(messages(contractorName))
-      doc.select("dt").text must include(messages("submittedNoReceipt.details.employersReference"))
-      doc.select("dd").text must include(messages(empRef))
-      doc.select("dt").text must include(messages("submittedNoReceipt.details.emailConfirmationSentTo"))
-      doc.select("dd").text must include(messages(email))
 
-      doc.select("h2").text                     must include(messages("submittedNoReceipt.h2"))
-      doc.select("p").text                      must include(messages("submittedNoReceipt.p2"))
-      doc.select("p").text                      must include(
-        messages(
-          s"We were unable to issue a submission receipt at this time. HMRC will contact you shortly. " +
-            s"You will receive confirmation of your submission at the following email address: $email"
-        )
+      doc.select("p.govuk-body").text must include(
+        messages("monthlyreturns.submittedNoReceipt.submissionHistory.p")
       )
-      doc.select("p").text                      must include(messages("submittedNoReceipt.p4"))
-      doc.getElementsByClass("govuk-link").text must include(messages("submittedNoReceipt.p4.link"))
+      doc.select("a.govuk-link").text must include(
+        messages("monthlyreturns.submittedNoReceipt.submissionHistory.link")
+      )
+
+      doc.select("a.govuk-link").text must include(
+        messages("monthlyreturns.submittedNoReceipt.backToManageYourCISReturn.link")
+      )
+
+      doc.select("p.govuk-body").text must include(
+        messages("monthlyreturns.submittedNoReceipt.questionsAboutReturnSubmission.p")
+      )
+      doc.select("a.govuk-link").text must include(
+        messages("monthlyreturns.submittedNoReceipt.questionsAboutReturnSubmission.link")
+      )
+
+      doc.select("h2").text must include(
+        messages("monthlyreturns.submittedNoReceipt.feedback.heading")
+      )
+
+      doc.select("p.govuk-body").text must include(
+        messages("monthlyreturns.submittedNoReceipt.feedback.p1")
+      )
+      doc.select("p.govuk-body").text must include(
+        messages("monthlyreturns.submittedNoReceipt.feedback.p2")
+      )
+
+      doc.select("a.govuk-link").text must include(
+        messages("monthlyreturns.submittedNoReceipt.feedback.p2.link")
+      )
     }
   }
 
   trait Setup {
-    val app: Application                          = applicationBuilder().build()
-    val view: SubmittedNoReceiptView              = app.injector.instanceOf[SubmittedNoReceiptView]
+
+    val app: Application             = applicationBuilder().build()
+    val view: SubmittedNoReceiptView = app.injector.instanceOf[SubmittedNoReceiptView]
+
     implicit val request: play.api.mvc.Request[_] = FakeRequest()
-    implicit val messages: Messages               = play.api.i18n.MessagesImpl(
-      play.api.i18n.Lang.defaultLang,
-      app.injector.instanceOf[play.api.i18n.MessagesApi]
+    implicit val messages: Messages               =
+      play.api.i18n.MessagesImpl(
+        play.api.i18n.Lang.defaultLang,
+        app.injector.instanceOf[play.api.i18n.MessagesApi]
+      )
+
+    val periodEnd                  = "February 2026"
+    val submittedTime              = "10:30am"
+    val submittedDate              = "6 Jan 2026"
+    val contractorName             = "Test Contractor Ltd"
+    val empRef                     = "123/AB456"
+    val email                      = "test@test.com"
+    val submissionType: ReturnType = ReturnType.MonthlyNilReturn
+
+    val html: HtmlFormat.Appendable = view(
+      periodEnd = periodEnd,
+      submittedTime = submittedTime,
+      submittedDate = submittedDate,
+      contractorName = contractorName,
+      empRef = empRef,
+      email = email,
+      submissionType = submissionType
     )
-
-    private val london                = ZoneId.of("Europe/London")
-    private val fixedInstant: Instant = Instant.parse("2017-01-06T08:46:00Z")
-    private val ukNow: ZonedDateTime  = ZonedDateTime.ofInstant(fixedInstant, london)
-    private val dmyFmt                = DateTimeFormatter.ofPattern("d MMM uuuu")
-    private val timeFmt               = DateTimeFormatter.ofPattern("HH:mm z")
-    private val year                  = 2018
-    private val month                 = 3
-    private val dayOfMonth            = 5
-
-    val periodEnd: String     = LocalDate.of(year, month, dayOfMonth).format(dmyFmt)
-    val submittedTime: String = ukNow.format(timeFmt)
-    val submittedDate: String = ukNow.format(dmyFmt)
-    val contractorName        = "PAL 355 Scheme"
-    val empRef                = "123/AB456"
-    val email                 = "test@test.com"
   }
 }
