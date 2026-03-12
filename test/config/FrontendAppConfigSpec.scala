c71e38ca0422abd5e1286f127d17c8e849ba697a
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2026-03-12 10:47:33 +0000

    DTR-3678: config updated

--- test/config/FrontendAppConfigSpec.scala
+++ test/config/FrontendAppConfigSpec.scala
@@ -6,8 +6,8 @@ import play.api.Application
 class FrontendAppConfigSpec extends SpecBase {
   "FrontendAppConfig" - {
     "must contain correct values for the provided configuration" in new Setup {
-      appConfig.manageYourReturnDashboardUrl mustBe
-        "http://localhost:6996/construction-industry-scheme/management/org/cis-return-dashboard/target/returnHistory"
+      appConfig.manageYourCisReturn mustBe
+        "http://localhost:6996/construction-industry-scheme/management/manage-cis-return/"
     }
   }
 
