9262b3f89603a4e715bc3c12118c12a92444cd4b
jamalosman <7947206+jamalosman@users.noreply.github.com>
2026-03-17 11:58:20 +0000

    DTR-3964: format

--- app/pages/monthlyreturns/AddSubcontractorDetailsPage.scala
+++ app/pages/monthlyreturns/AddSubcontractorDetailsPage.scala
@@ -21,7 +21,7 @@ import play.api.libs.json.JsPath
 
 case object AddSubcontractorDetailsPage extends QuestionPage[String] {
 
-  override def path: JsPath = JsPath \ toString
+        override def path: JsPath = JsPath \ toString
 
   override def toString: String = "addSubcontractorDetails"
 }
