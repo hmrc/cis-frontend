b10b5dc04bb19a3105812af4c723f9893c91779b
wg-hmrc <107496826+wg-hmrc@users.noreply.github.com>
2026-04-27 11:02:43 +0100

    [DTR-4547] CIS AMR Screen Ticket MRAR05 - Which subcontractors do you want to select? (#182)
    * [DTR-4547] Adding Which subcontractors do you want to select? screen
    
    * [DTR-4547] formating fix
    
    * [DTR-4547] Sort subcontractors by name and add pre-selected checkboxes

---
+++ app/models/amend/WhichSubcontractorsToAdd.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.amend
+
+import play.api.libs.json.{Json, OFormat}
+import uk.gov.hmrc.govukfrontend.views.viewmodels.checkboxes.CheckboxItem
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.Text
+import viewmodels.govuk.checkbox._
+
+case class Subcontractor(id: String, name: String)
+
+object Subcontractor {
+  implicit val format: OFormat[Subcontractor] = Json.format[Subcontractor]
+}
+
+object WhichSubcontractorsToAdd {
+
+  // TODO: Replace with real data from backend
+  val mockSubcontractors: Seq[Subcontractor] = Seq(
+    Subcontractor("1", "Alice, A"),
+    Subcontractor("2", "Apex Construction Solutions"),
+    Subcontractor("3", "Bob, B"),
+    Subcontractor("4", "Bloggs, Joe"),
+    Subcontractor("5", "Bloggs, Joseph"),
+    Subcontractor("6", "Build Right Construction"),
+    Subcontractor("7", "Charles, C"),
+    Subcontractor("8", "Dave, D"),
+    Subcontractor("9", "Draft Services Ltd"),
+    Subcontractor("10", "Elise, E"),
+    Subcontractor("11", "Frank, F"),
+    Subcontractor("12", "Northern Trades Ltd"),
+    Subcontractor("13", "Pro-Build Subcontractors"),
+    Subcontractor("14", "Tynewear Ltd"),
+    Subcontractor("15", "SubbyCo Ltd")
+  )
+
+  val mockPreSelectedIds: Set[String] = Set("2", "4", "6", "12", "15")
+
+  def checkboxItems(subcontractors: Seq[Subcontractor], preSelectedIds: Set[String] = Set.empty): Seq[CheckboxItem] =
+    subcontractors.sortBy(_.name).zipWithIndex.map { case (sub, index) =>
+      CheckboxItemViewModel(
+        content = Text(sub.name),
+        fieldId = "value",
+        index = index,
+        value = sub.id
+      ).copy(checked = preSelectedIds.contains(sub.id))
+    }
+}
---
+++ test/models/amend/WhichSubcontractorsToAddSpec.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.amend
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.OptionValues
+import play.api.libs.json.Json
+import uk.gov.hmrc.govukfrontend.views.viewmodels.checkboxes.CheckboxItem
+
+class WhichSubcontractorsToAddSpec extends AnyFreeSpec with Matchers with OptionValues {
+
+  "Subcontractor" - {
+
+    "must serialise and deserialise" in {
+      val sub  = Subcontractor("1", "Alice, A")
+      val json = Json.toJson(sub)
+      json.as[Subcontractor] mustEqual sub
+    }
+  }
+
+  "WhichSubcontractorsToAdd" - {
+
+    "mockSubcontractors must not be empty" in {
+      WhichSubcontractorsToAdd.mockSubcontractors must not be empty
+    }
+
+    "checkboxItems must return one item per subcontractor sorted by name" in {
+      val subs  = Seq(Subcontractor("2", "Bob, B"), Subcontractor("1", "Alice, A"))
+      val items = WhichSubcontractorsToAdd.checkboxItems(subs)
+
+      items.length mustEqual 2
+      items.head.value mustEqual "1"
+      items(1).value mustEqual "2"
+    }
+
+    "checkboxItems must mark pre-selected items as checked" in {
+      val subs  = Seq(Subcontractor("1", "Alice, A"), Subcontractor("2", "Bob, B"))
+      val items = WhichSubcontractorsToAdd.checkboxItems(subs, Set("2"))
+
+      items.head.checked mustEqual false
+      items(1).checked mustEqual true
+    }
+
+    "checkboxItems must return items of type CheckboxItem" in {
+      val subs  = WhichSubcontractorsToAdd.mockSubcontractors
+      val items = WhichSubcontractorsToAdd.checkboxItems(subs)
+
+      items.foreach(_ mustBe a[CheckboxItem])
+    }
+  }
+}
