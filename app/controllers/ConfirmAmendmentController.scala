51487718f784a20c325f11bc368de90904bce025
samraducan <231910514+samraducan@users.noreply.github.com>
2026-04-27 14:27:35 +0100

    DTR-4536: Add page content and unit tests

--- app/controllers/ConfirmAmendmentController.scala
+++ app/controllers/ConfirmAmendmentController.scala
@@ -23,17 +23,17 @@ import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.ConfirmAmendmentView
 
-class ConfirmAmendmentController @Inject()(
-                                       override val messagesApi: MessagesApi,
-                                       identify: IdentifierAction,
-                                       getData: DataRetrievalAction,
-                                       requireData: DataRequiredAction,
-                                       val controllerComponents: MessagesControllerComponents,
-                                       view: ConfirmAmendmentView
-                                     ) extends FrontendBaseController with I18nSupport {
+class ConfirmAmendmentController @Inject() (
+  override val messagesApi: MessagesApi,
+  identify: IdentifierAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  val controllerComponents: MessagesControllerComponents,
+  view: ConfirmAmendmentView
+) extends FrontendBaseController
+    with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
-    implicit request =>
-      Ok(view())
+  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
+    Ok(view())
   }
 }
