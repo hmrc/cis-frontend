1c9245d4f368acb57dd764e6a5214972a156ff64
wg-hmrc <107496826+wg-hmrc@users.noreply.github.com>
2026-04-28 14:57:58 +0100

    [DTR-4547-bugfix] resolve checkbox form binding (#191)

--- test/controllers/amend/WhichSubcontractorsToAddControllerSpec.scala
+++ test/controllers/amend/WhichSubcontractorsToAddControllerSpec.scala
@@ -123,6 +123,32 @@ class WhichSubcontractorsToAddControllerSpec extends SpecBase with MockitoSugar 
       }
     }
 
+    "must redirect to the next page when multiple checkboxes are selected" in {
+
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+          .overrides(
+            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, whichSubcontractorsToAddRoute)
+            .withFormUrlEncodedBody(("value[]", subcontractors.head.id), ("value[]", subcontractors(1).id))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onwardRoute.url
+      }
+    }
+
     "must return a Bad Request and errors when invalid data is submitted" in {
 
       val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
