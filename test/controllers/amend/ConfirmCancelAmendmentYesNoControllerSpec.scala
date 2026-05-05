170cc79a0097da3c55ab320f4eb40b5592250ffe
Yohan Jayarathna <129070827+yohan-oc@users.noreply.github.com>
2026-05-05 10:12:52 +0100

    DTR-4550: Screen Ticket MR02 - Cancel amendment (#190)

---
+++ test/controllers/amend/ConfirmCancelAmendmentYesNoControllerSpec.scala
@@ -0,0 +1,144 @@
+package controllers.amend
+
+import base.SpecBase
+import forms.amend.ConfirmCancelAmendmentYesNoFormProvider
+import models.UserAnswers
+import navigation.{FakeNavigator, Navigator}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import pages.amend.ConfirmCancelAmendmentYesNoPage
+import play.api.data.Form
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import repositories.SessionRepository
+import views.html.amend.ConfirmCancelAmendmentYesNoView
+
+import scala.concurrent.Future
+
+class ConfirmCancelAmendmentYesNoControllerSpec extends SpecBase with MockitoSugar {
+
+  def onwardRoute = Call("GET", "/foo")
+
+  val formProvider        = new ConfirmCancelAmendmentYesNoFormProvider()
+  val form: Form[Boolean] = formProvider()
+
+  lazy val confirmCancelAmendmentYesNoRoute: String = routes.ConfirmCancelAmendmentYesNoController.onPageLoad().url
+
+  private val monthYear: String = "April 2026"
+
+  "ConfirmCancelAmendmentYesNo Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, confirmCancelAmendmentYesNoRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[ConfirmCancelAmendmentYesNoView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form, monthYear)(request, messages(application)).toString
+      }
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = UserAnswers(userAnswersId).set(ConfirmCancelAmendmentYesNoPage, true).success.value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, confirmCancelAmendmentYesNoRoute)
+
+        val view = application.injector.instanceOf[ConfirmCancelAmendmentYesNoView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form.fill(true), monthYear)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+          .overrides(
+            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, confirmCancelAmendmentYesNoRoute)
+            .withFormUrlEncodedBody(("value", "true"))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onwardRoute.url
+      }
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, confirmCancelAmendmentYesNoRoute)
+            .withFormUrlEncodedBody(("value", ""))
+
+        val boundForm = form.bind(Map("value" -> ""))
+
+        val view = application.injector.instanceOf[ConfirmCancelAmendmentYesNoView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) mustEqual view(boundForm, monthYear)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request = FakeRequest(GET, confirmCancelAmendmentYesNoRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery for a POST if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, confirmCancelAmendmentYesNoRoute)
+            .withFormUrlEncodedBody(("value", "true"))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+  }
+}
---
+++ test/views/amend/ConfirmCancelAmendmentYesNoViewSpec.scala
@@ -0,0 +1,81 @@
+package views.amend
+
+import base.SpecBase
+import forms.amend.ConfirmCancelAmendmentYesNoFormProvider
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.api.Application
+import play.api.data.Form
+import play.api.i18n.{Lang, Messages}
+import play.api.test.FakeRequest
+import play.twirl.api.HtmlFormat
+import views.html.amend.ConfirmCancelAmendmentYesNoView
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+class ConfirmCancelAmendmentYesNoViewSpec extends SpecBase {
+
+  "ConfirmCancelAmendmentYesNoView" - {
+
+    "must render the page with the correct html elements" in new Setup {
+      val doc: Document = Jsoup.parse(html.toString)
+
+      doc.title must include(messages("amend.confirmCancelAmendmentYesNo.title", monthYear))
+      doc.title must include(monthYear)
+
+      doc.select("h1").text must include(messages("amend.confirmCancelAmendmentYesNo.heading", monthYear))
+      doc.select("h1").text must include(monthYear)
+
+      doc.getElementsByClass("govuk-button").text must include(messages("site.continue"))
+    }
+
+    "must render radio buttons with correct values" in new Setup {
+      val doc: Document = Jsoup.parse(html.toString)
+
+      doc.select("input[value=true]").size() mustBe 1
+      doc.select("input[value=false]").size() mustBe 1
+
+      doc.select("label").text() must include(messages("site.yes"))
+      doc.select("label").text() must include(messages("site.no"))
+    }
+
+    "must pre-populate the form when user has previously answered 'true'" in new Setup {
+      val filledForm: Form[Boolean]         = form.fill(true)
+      val filledHtml: HtmlFormat.Appendable = view(filledForm, monthYear)(request, messages)
+      val doc: Document                     = Jsoup.parse(filledHtml.toString)
+
+      doc.select("input[value=true]").hasAttr("checked") mustBe true
+      doc.select("input[value=false]").hasAttr("checked") mustBe false
+    }
+
+    "must pre-populate the form when user has previously answered 'false'" in new Setup {
+      val filledForm: Form[Boolean]         = form.fill(false)
+      val filledHtml: HtmlFormat.Appendable = view(filledForm, monthYear)(request, messages)
+      val doc: Document                     = Jsoup.parse(filledHtml.toString)
+
+      doc.select("input[value=true]").hasAttr("checked") mustBe false
+      doc.select("input[value=false]").hasAttr("checked") mustBe true
+    }
+  }
+
+  trait Setup {
+    val app: Application                          = applicationBuilder().build()
+    val view: ConfirmCancelAmendmentYesNoView     = app.injector.instanceOf[ConfirmCancelAmendmentYesNoView]
+    val formProvider                              = new ConfirmCancelAmendmentYesNoFormProvider()
+    val form: Form[Boolean]                       = formProvider()
+    implicit val request: play.api.mvc.Request[_] = FakeRequest()
+
+    private val messagesApi = app.injector.instanceOf[play.api.i18n.MessagesApi]
+
+    val messages: Messages = play.api.i18n.MessagesImpl(Lang("en"), messagesApi)
+
+    private val monthYearDate: LocalDate = LocalDate.of(2026, 4, 1)
+
+    val monthYear: String =
+      monthYearDate.format(DateTimeFormatter.ofPattern("MMMM uuuu").withLocale(Locale.UK))
+
+    val html: HtmlFormat.Appendable = view(form, monthYear)(request, messages)
+  }
+}
