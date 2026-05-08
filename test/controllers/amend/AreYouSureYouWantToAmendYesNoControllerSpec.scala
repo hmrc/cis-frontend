c8241a459964aa06f16e6d2755b90333da8220ea
wg-hmrc <107496826+wg-hmrc@users.noreply.github.com>
2026-05-08 12:37:50 +0100

    [DTR-4546] CIS AMR Screen Ticket MRAR09 - Are you sure you want to amend this return to a nil return? (#189)
    * [DTR-4546] Adding scaffolding
    
    * [DTR-4546] Adding final changes
    
    * [DTR-4546] Resolving PR comment
    
    * [DTR-4546] resolving PR comments
    
    * [DTR-4546] fixing failed test

---
+++ test/controllers/amend/AreYouSureYouWantToAmendYesNoControllerSpec.scala
@@ -0,0 +1,167 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.amend
+
+import base.SpecBase
+import forms.amend.AreYouSureYouWantToAmendYesNoFormProvider
+import models.UserAnswers
+import models.amend.AreYouSureYouWantToAmendYesNo
+import navigation.{FakeNavigator, Navigator}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import pages.amend.AreYouSureYouWantToAmendYesNoPage
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import repositories.SessionRepository
+import views.html.amend.AreYouSureYouWantToAmendYesNoView
+
+import scala.concurrent.Future
+
+class AreYouSureYouWantToAmendYesNoControllerSpec extends SpecBase with MockitoSugar {
+
+  def onwardRoute = Call("GET", "/foo")
+
+  lazy val areYouSureYouWantToAmendYesNoRoute =
+    routes.AreYouSureYouWantToAmendYesNoController.onPageLoad().url
+
+  val formProvider = new AreYouSureYouWantToAmendYesNoFormProvider()
+  val form         = formProvider()
+
+  "AreYouSureYouWantToAmendYesNo Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, areYouSureYouWantToAmendYesNoRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[AreYouSureYouWantToAmendYesNoView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form)(request, messages(application)).toString
+      }
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers =
+        UserAnswers(userAnswersId)
+          .set(AreYouSureYouWantToAmendYesNoPage, AreYouSureYouWantToAmendYesNo.values.head)
+          .success
+          .value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, areYouSureYouWantToAmendYesNoRoute)
+
+        val view = application.injector.instanceOf[AreYouSureYouWantToAmendYesNoView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form.fill(AreYouSureYouWantToAmendYesNo.values.head))(
+          request,
+          messages(application)
+        ).toString
+      }
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+          .overrides(
+            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, areYouSureYouWantToAmendYesNoRoute)
+            .withFormUrlEncodedBody(("value", AreYouSureYouWantToAmendYesNo.values.head.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onwardRoute.url
+      }
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, areYouSureYouWantToAmendYesNoRoute)
+            .withFormUrlEncodedBody(("value", "invalid value"))
+
+        val boundForm = form.bind(Map("value" -> "invalid value"))
+
+        val view = application.injector.instanceOf[AreYouSureYouWantToAmendYesNoView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) mustEqual view(boundForm)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request = FakeRequest(GET, areYouSureYouWantToAmendYesNoRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "redirect to Journey Recovery for a POST if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, areYouSureYouWantToAmendYesNoRoute)
+            .withFormUrlEncodedBody(("value", AreYouSureYouWantToAmendYesNo.values.head.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+
+        redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+  }
+}
