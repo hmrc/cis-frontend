4df999665e1b9b83140292bf5b085c0c49f275f5
wg-hmrc <waigwa.gatamu@digital.hmrc.gov.uk>
2026-06-03 10:36:51 +0100

    [DTR-5615] Resolving PR comments

--- app/controllers/finalvalidations/ReviewSubcontractorDetailsController.scala
+++ app/controllers/finalvalidations/ReviewSubcontractorDetailsController.scala
@@ -14,14 +14,14 @@
  * limitations under the License.
  */
 
-package controllers
+package controllers.finalvalidations
 
 import controllers.actions._
 import javax.inject.Inject
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import views.html.ReviewSubcontractorDetailsView
+import views.html.finalvalidations.ReviewSubcontractorDetailsView
 
 class ReviewSubcontractorDetailsController @Inject() (
   override val messagesApi: MessagesApi,
--- test/views/finalvalidations/ReviewSubcontractorDetailsViewSpec.scala
+++ test/views/finalvalidations/ReviewSubcontractorDetailsViewSpec.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package views
+package views.finalvalidations
 
 import base.SpecBase
 import controllers.monthlyreturns.routes.SelectSubcontractorsController
@@ -24,22 +24,22 @@ import play.api.Application
 import play.api.i18n.{Lang, Messages, MessagesApi, MessagesImpl}
 import play.api.test.FakeRequest
 import play.twirl.api.HtmlFormat
-import views.html.ReviewSubcontractorDetailsView
+import views.html.finalvalidations.ReviewSubcontractorDetailsView
 
 class ReviewSubcontractorDetailsViewSpec extends SpecBase {
 
   "ReviewSubcontractorDetailsView" - {
 
     "must render the correct page title" in new Setup {
-      doc.title must include(messages("reviewSubcontractorDetails.title"))
+      doc.title must include(messages("finalValidations.reviewSubcontractorDetails.title"))
     }
 
     "must render the correct heading" in new Setup {
-      doc.select("h1").text mustEqual messages("reviewSubcontractorDetails.heading")
+      doc.select("h1").text mustEqual messages("finalValidations.reviewSubcontractorDetails.heading")
     }
 
     "must render the intro paragraph" in new Setup {
-      doc.select("p.govuk-body").text must include(messages("reviewSubcontractorDetails.intro"))
+      doc.select("p.govuk-body").text must include(messages("finalValidations.reviewSubcontractorDetails.intro"))
     }
 
     "must render each subcontractor as a task list item link" in new Setup {
@@ -60,7 +60,7 @@ class ReviewSubcontractorDetailsViewSpec extends SpecBase {
       val tags = doc.select(".govuk-task-list__status .govuk-tag").eachText()
 
       tags.size mustEqual subcontractors.size
-      tags.forEach(_ mustEqual messages("reviewSubcontractorDetails.taskList.status.incomplete"))
+      tags.forEach(_ mustEqual messages("finalValidations.reviewSubcontractorDetails.taskList.status.incomplete"))
     }
 
     "must render File a return as the last task list item without a link" in new Setup {
@@ -68,7 +68,7 @@ class ReviewSubcontractorDetailsViewSpec extends SpecBase {
       val lastItem      = taskListItems.last()
 
       lastItem.select(".govuk-task-list__name-and-hint").text mustEqual messages(
-        "reviewSubcontractorDetails.taskList.fileAReturn"
+        "finalValidations.reviewSubcontractorDetails.taskList.fileAReturn"
       )
       lastItem.select("a").isEmpty mustBe true
     }
@@ -77,14 +77,16 @@ class ReviewSubcontractorDetailsViewSpec extends SpecBase {
       val taskListItems = doc.select(".govuk-task-list__item")
       val lastStatus    = taskListItems.last().select(".govuk-task-list__status")
 
-      lastStatus.text mustEqual messages("reviewSubcontractorDetails.taskList.status.cannotContinueYet")
+      lastStatus.text mustEqual messages(
+        "finalValidations.reviewSubcontractorDetails.taskList.status.cannotContinueYet"
+      )
       lastStatus.hasClass("govuk-task-list__status--cannot-start-yet") mustBe true
     }
 
     "must render the back link with the correct URL" in new Setup {
       val backLink = doc.select("p.govuk-body > a.govuk-link").last()
 
-      backLink.text mustEqual messages("reviewSubcontractorDetails.backLink")
+      backLink.text mustEqual messages("finalValidations.reviewSubcontractorDetails.backLink")
       backLink.attr("href") mustEqual SelectSubcontractorsController.onPageLoad(None).url
     }
 
@@ -94,7 +96,7 @@ class ReviewSubcontractorDetailsViewSpec extends SpecBase {
 
       taskListItems.size mustEqual 1
       taskListItems.first().select(".govuk-task-list__name-and-hint").text mustEqual messages(
-        "reviewSubcontractorDetails.taskList.fileAReturn"
+        "finalValidations.reviewSubcontractorDetails.taskList.fileAReturn"
       )
     }
   }
