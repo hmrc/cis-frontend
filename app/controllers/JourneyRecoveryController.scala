16b113ff75ff1e81dcd9ae8b5ce68986f41e6e7d
Jesse-Murray-Dev <209797357+Jesse-Murray-Dev@users.noreply.github.com>
2026-08-03 11:30:30 +0100

    Feat(DTR-7045): JourneyRecoveryStartAgain accountType dynamic link

--- app/controllers/JourneyRecoveryController.scala
+++ app/controllers/JourneyRecoveryController.scala
@@ -17,12 +17,12 @@
 package controllers
 
 import config.FrontendAppConfig
-import controllers.actions.IdentifierAction
+import controllers.actions.{CisIdRequiredAction, DataRequiredAction, DataRetrievalAction, IdentifierAction}
 import play.api.Logging
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import uk.gov.hmrc.play.bootstrap.binders.RedirectUrl._
-import uk.gov.hmrc.play.bootstrap.binders._
+import uk.gov.hmrc.play.bootstrap.binders.RedirectUrl.*
+import uk.gov.hmrc.play.bootstrap.binders.*
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.{JourneyRecoveryContinueView, JourneyRecoveryStartAgainView}
 
@@ -31,6 +31,9 @@ import javax.inject.Inject
 class JourneyRecoveryController @Inject() (
   val controllerComponents: MessagesControllerComponents,
   identify: IdentifierAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  requireCisId: CisIdRequiredAction,
   continueView: JourneyRecoveryContinueView,
   startAgainView: JourneyRecoveryStartAgainView
 )(implicit appConfig: FrontendAppConfig)
@@ -38,7 +41,15 @@ class JourneyRecoveryController @Inject() (
     with I18nSupport
     with Logging {
 
-  def onPageLoad(continueUrl: Option[RedirectUrl] = None): Action[AnyContent] = identify { implicit request =>
+  def onPageLoad(continueUrl: Option[RedirectUrl] = None): Action[AnyContent] = (identify andThen getData andThen
+    requireData andThen requireCisId) { implicit request =>
+
+    val cisAccountUrl =
+      if (request.isAgent) {
+        s"${appConfig.constructionIndustryAgentAccountUrl}${request.cisId}"
+      } else {
+        appConfig.constructionIndustryOrgAccountUrl
+      }
 
     val safeUrl: Option[String] = continueUrl.flatMap { unsafeUrl =>
       unsafeUrl.getEither(OnlyRelative) match {
@@ -52,6 +63,6 @@ class JourneyRecoveryController @Inject() (
 
     safeUrl
       .map(url => Ok(continueView(url)))
-      .getOrElse(Ok(startAgainView()))
+      .getOrElse(Ok(startAgainView(cisAccountUrl)))
   }
 }
--- test/views/JourneyRecoveryStartAgainViewSpec.scala
+++ test/views/JourneyRecoveryStartAgainViewSpec.scala
@@ -27,33 +27,41 @@ class JourneyRecoveryStartAgainViewSpec extends SpecBase with Matchers {
 
   "JourneyRecoveryStartAgainView" - {
 
-    "must render the page with the correct heading and paragraph with link" in new Setup {
-      val html = view()
-      val doc  = Jsoup.parse(html.body)
+    Seq(
+      ("AGENT", applicationConfig.constructionIndustryAgentAccountUrl + "1"),
+      ("ORGANISATION", applicationConfig.constructionIndustryOrgAccountUrl)
+    ).foreach { case (accountTypeSTR, cisAccountURL) =>
+      s"when accountType is '$accountTypeSTR'" - {
 
-      doc.title                                 must include(messages("journeyRecovery.startAgain.title"))
-      doc.select("h1").text                     must include(messages("journeyRecovery.startAgain.heading"))
-      doc.select("p").text                      must include(messages("journeyRecovery.startAgain.guidance.p1"))
-      doc.select("p").text                      must include(messages("journeyRecovery.startAgain.guidance.p2"))
-      doc.select("p").text                      must include(messages("journeyRecovery.startAgain.guidance.contactHMRC.suffix"))
-      doc.getElementsByClass("govuk-link").text must include(
-        messages("journeyRecovery.startAgain.guidance.contactHMRC.link")
-      )
-      doc.select("p").text                      must include(messages("journeyRecovery.startAgain.guidance.cisAccount.prefix"))
-      doc.getElementsByClass("govuk-link").text must include(
-        messages("journeyRecovery.startAgain.guidance.cisAccount.link")
-      )
+        "must render the page with the correct heading and paragraph with link" in new Setup {
+          val html = view(cisAccountURL)
+          val doc  = Jsoup.parse(html.body)
 
+          doc.title                                 must include(messages("journeyRecovery.startAgain.title"))
+          doc.select("h1").text                     must include(messages("journeyRecovery.startAgain.heading"))
+          doc.select("p").text                      must include(messages("journeyRecovery.startAgain.guidance.p1"))
+          doc.select("p").text                      must include(messages("journeyRecovery.startAgain.guidance.p2"))
+          doc.select("p").text                      must include(messages("journeyRecovery.startAgain.guidance.contactHMRC.suffix"))
+          doc.getElementsByClass("govuk-link").text must include(
+            messages("journeyRecovery.startAgain.guidance.contactHMRC.link")
+          )
+          doc.select("p").text                      must include(messages("journeyRecovery.startAgain.guidance.cisAccount.prefix"))
+          doc.getElementsByClass("govuk-link").text must include(
+            messages("journeyRecovery.startAgain.guidance.cisAccount.link")
+          )
+
+        }
+      }
+
+      trait Setup {
+        val app                                       = applicationBuilder().build()
+        val view                                      = app.injector.instanceOf[JourneyRecoveryStartAgainView]
+        implicit val request: play.api.mvc.Request[_] = FakeRequest()
+        implicit val messages: Messages               = play.api.i18n.MessagesImpl(
+          play.api.i18n.Lang.defaultLang,
+          app.injector.instanceOf[play.api.i18n.MessagesApi]
+        )
+      }
     }
-  }
-
-  trait Setup {
-    val app                                       = applicationBuilder().build()
-    val view                                      = app.injector.instanceOf[JourneyRecoveryStartAgainView]
-    implicit val request: play.api.mvc.Request[_] = FakeRequest()
-    implicit val messages: Messages               = play.api.i18n.MessagesImpl(
-      play.api.i18n.Lang.defaultLang,
-      app.injector.instanceOf[play.api.i18n.MessagesApi]
-    )
   }
 }
