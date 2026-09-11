dfad310d427e85ca6d56f1484a4c53f82d645f72
codeneto-hmrc <253322371+codeneto-hmrc@users.noreply.github.com>
2026-09-11 11:24:22 +0100

    DTR-4152: Add Feedback Component to CIS Confirmation Pages

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -63,6 +63,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   private lazy val exitSurveyBaseUrl: String =
     configuration.get[Service]("microservice.services.feedback-frontend").baseUrl
   lazy val exitSurveyUrl: String             = s"$exitSurveyBaseUrl/feedback/cis-frontend"
+  lazy val cisFeedbackSurveyUrl: String      = s"$exitSurveyBaseUrl/feedback/construction-industry-scheme"
 
   lazy val languageTranslationEnabled: Boolean =
     configuration.get[Boolean]("features.welsh-translation")
