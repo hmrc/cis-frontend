8d999094b32d793cb86ac7d09aff91f52839ec96
Richy Jassal <20478717+jassalrichy@users.noreply.github.com>
2026-09-18 15:07:45 +0100

    [DTR-8647] PRA - Update README

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -57,7 +57,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   lazy val taxAgentsAndAdvisorsAuthorisationFormsUrl: String =
     configuration.get[String]("urls.taxAgentsAndAdvisorsAuthorisationForms")
   lazy val clientListSearchUrl: String                       = configuration.get[String]("urls.clientListSearch")
- 
+
   lazy val verifySubcontractorsUrl: String = configuration.get[String]("urls.verifySubcontractors")
 
   private lazy val manageSubcontractorsBaseUrl: String =
